ebd4c49c10b50292b23414757487d810f9f7e4a7
shriyashshukla <harishukla797@gmail.com>
2023-11-22 14:47:14 +0530

    ...

---
+++ code.py
@@ -0,0 +1,28 @@
+print("helloo world")
+print ("this is my first program")
+
+
+# from playsound import playsound
+# playsound ('D:\\ Wed download \\ Habibi Cat - Millionare Billi #arabic #cats #shorts [TubeRipper.com] (1).mp3 ')
+
+
+a="shriyash shukla"
+b="shriyash shukla"
+c= 345
+d= 45.32
+e= True
+f= None
+# printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+# printing the type of variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(type(f)))
+
---
+++ testing.py
@@ -0,0 +1,17 @@
+
+multiline_string = """
+This is a multiline string.
+It can span multiple lines.
+You can include line breaks and indentation.
+"""
+
+print(multiline_string)
+
+#add to number 
+a= 45
+b= 55
+print("the sum of a and b is", a+b)
+# user input
+name = input("enter your name")
+print("good afternoon", name)
+
