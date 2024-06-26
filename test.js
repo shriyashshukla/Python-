c3cb9de75f9fe0b8dd02d9ca8f9122354741cc45
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-06-26 12:45:25 +0530

    revision

--- test.js
+++ test.js
@@ -1,1 +1,105 @@
 console.log("only for testing ")
+
+
+
+
+
+
+let a =1
+var c =3
+let b =2
+let d = BigInt("897")
+const author ="shriyash shukla"
+{
+    let a="4"
+    console.log(a)
+
+}
+
+console.log(a)
+console.log(c)
+console.log(b)
+console.log(d)
+console.log(typeof d)
+console.log(author)
+
+
+const item = {
+     "Biscuit" : true,
+     "Regular" :67,
+    price : 45
+}
+
+
+console.log(item["Regular"])
+
+// add two 
+let i = "hari "
+let j= "shukla"
+
+console.log(typeof(i+j))
+console.log(i+j)
+
+
+const a1 ={
+    name:"hari",
+    section:2,
+    roll: 45
+}
+
+a1['friend']= "shubham"
+a1['roll']= 46
+ 
+console.log(a1)
+
+console.log("operatores in js ")
+let x = 13
+let y = 6
+console.log("1 =",x+y)
+console.log("2= ",x-y)
+console.log(x*y)
+console.log(x/y)
+console.log("3=",x%y)
+console.log(x**y)
+console.log(x++)
+console.log(x--)
+console.log(++x)
+console.log(--x)
+console.log(x==y)
+console.log(x!=y)
+console.log(x>y)
+console.log(x<y)
+console.log(x>=y)
+console.log(x<=y)
+console.log(x&&y)
+console.log(x||y)
+console.log(!x)
+console.log(x===y)
+console.log(x!==y)
+console.log(x+=y)
+console.log(x-=y)
+console.log(x*=y)
+console.log(x/=y)
+console.log(x%=y)
+console.log(x**=y)
+console.log(x&=y)
+console.log(x|=y)
+console.log(x^=y)
+console.log(x>>=y)
+console.log(x<<=y)
+console.log(x>>>=y)
+console.log(x&&=y)
+console.log(x||=y)
+console.log(x??y)
+console.log(x??=y)
+
+
+console.log("type of operator")
+let a2="2"
+
+console.log(a2)
+console.log(a2++)
+console.log(++a2)
+
+
+console.log("conditional operator")
