969167c87c5875e5e91104feff362db5f5098e82
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-07-05 22:21:25 +0530

    chore: Refactor code to use async/await for fetching data

---
+++ js/await.js
@@ -0,0 +1,13 @@
+async function fetchData(){
+    try{
+        const response =await fetch ('https://api.example.com/data');
+        const data = await response.json();
+        console.log(data)
+
+    }
+    catch(error){
+        console.log('Error:',error)
+    }
+}
+
+fetchData()
---
+++ js/webapi.js
@@ -0,0 +1,5 @@
+fetch('https://api.example.com/data')
+
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error:',error));
